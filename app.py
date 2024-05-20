e1275d62265c8730e1b282ca820a7b3699a01ca4
Min Thura Zaw <minthurazaw619@gmail.com>
2024-05-20 11:13:23 +0700

    update html files

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-todos = []
 
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    return render_template('login.html')
+
+
+@app.route('/home')
+def welcome():
+    return render_template('home.html')
+
+
+@app.route('/redirect_to_home', methods=['POST'])
+def redirect_to_welcome():
+    return redirect(url_for('welcome'))
 
 
 if __name__ == '__main__':
